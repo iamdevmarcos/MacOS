8d4226f92d4faae60c086385090bb11ed66441f8
Marcos Andre <marcosdev.me@gmail.com>
2021-12-22 09:43:06 -0300

    feat: create menuleftlist in other file

---
+++ src/utils/MenuList/MenuTopList.ts
@@ -0,0 +1,10 @@
+export const MenuTopList = [
+    {title: 'Mail'},
+    {title: 'File'},
+    {title: 'Edit'},
+    {title: 'View'},
+    {title: 'Mailbox'},
+    {title: 'Message'},
+    {title: 'Help'},
+    {title: 'Github'},
+];
