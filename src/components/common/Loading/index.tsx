9e5dc61189439d3488427a94934ece7e4ff688cd
Marcos Andre <marcosdev.me@gmail.com>
2021-12-22 13:00:54 -0300

    style: creating styles

--- src/components/common/Loading/index.tsx
+++ src/components/common/Loading/index.tsx
@@ -3,7 +3,10 @@ import * as C from './styles';
 export const Loading = () => {
     return (
         <C.Container>
-            ...
+            <div className="loadingArea">
+                <img src="/assets/images/apple-logo.png" alt="logo" />
+                <p>Loading</p>
+            </div>
         </C.Container>
     );
 }
--- src/components/common/Loading/styles.ts
+++ src/components/common/Loading/styles.ts
@@ -1,5 +1,26 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    
+    background-color:#000;
+    min-height:100vh;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+
+    .loadingArea {
+        display:flex;
+        flex-direction:column;
+        align-items:center;
+        justify-content:center;
+    }
+
+    img {
+        width:100px;
+        height:100px;
+    }
+
+    p {
+        margin-top:50px;
+        color:#FFF;
+    }
 `;
