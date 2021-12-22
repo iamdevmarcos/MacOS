046f787a29b1de0d8f1cae8c1e738ea3b87935da
Marcos Andre <marcosdev.me@gmail.com>
2021-12-22 12:47:34 -0300

    feat: adding loading in index

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import * as C from './App.styles';
 
+import { Loading } from './components/common/Loading';
 import { MenuIcon } from './components/screen/MenuIcon';
 
 import { MenuIconList } from './utils/MenuIconList';
@@ -8,6 +9,18 @@ import { MenuLeftItems } from './utils/MenuList/MenuLeftItems';
 import { MenuRightItems } from './utils/MenuList/MenuRightItems';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  if(loading) {
+    return (<Loading />);
+  }
+
   return (
     <C.Container>
       <C.MenuTop>
