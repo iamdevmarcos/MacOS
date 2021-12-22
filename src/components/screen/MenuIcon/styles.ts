be3dc8ff7a017a490b2776531520a2c12a6a3ade
Marcos Andre <marcosdev.me@gmail.com>
2021-12-22 14:18:50 -0300

    style: adding icon

--- src/components/screen/MenuIcon/styles.ts
+++ src/components/screen/MenuIcon/styles.ts
@@ -3,7 +3,9 @@ import styled from "styled-components";
 export const Container = styled.div`
     width:50px;
     height:50px;
-    background-color:#CCC;
+    background-image: url('/assets/images/icons/appicons/spotify.png');
+    background-position: center;
+    background-size:cover;
     margin:10px;
     cursor:pointer;
     transition: all ease 0.2s;
