a70ca385a7fb90b0ceec76f2846bbb1931847d00
Marcos Andre <marcosdev.me@gmail.com>
2021-12-28 21:39:55 -0300

    chore: adding draggable

--- src/components/common/Window/index.tsx
+++ src/components/common/Window/index.tsx
@@ -1,6 +1,8 @@
 import * as C from './styles';
 import { WindowIcon } from '../../../utils/WindowIcons';
 
+import Draggable from 'react-draggable';
+
 type Props = {
     close: (value: boolean) => void;
 }
@@ -9,24 +11,26 @@ export const Window = ({ close }: Props) => {
     const handleCloseWindow = () => close(false);
 
     return (
-        <C.Container>
-            <C.Top>
-                {WindowIcon.map((item, index) => (
-                    <div
-                        onClick={handleCloseWindow}
-                        style={{backgroundColor:`${item.bgColor}`}}
-                        key={index}
-                    >
-                        <img src={`/assets/images/icons/window/${item.title}.png`} alt="" />
-                    </div>
-                ))}
-            </C.Top>
+        <Draggable>
+            <C.Container>
+                <C.Top>
+                    {WindowIcon.map((item, index) => (
+                        <div
+                            onClick={handleCloseWindow}
+                            style={{backgroundColor:`${item.bgColor}`}}
+                            key={index}
+                        >
+                            <img src={`/assets/images/icons/window/${item.title}.png`} alt="" />
+                        </div>
+                    ))}
+                </C.Top>
 
-            <C.Body>
-                <div className="content">
-                    <p>Coming Soon!</p>
-                </div>
-            </C.Body>
-        </C.Container>
+                <C.Body>
+                    <div className="content">
+                        <p>Coming Soon!</p>
+                    </div>
+                </C.Body>
+            </C.Container>
+        </Draggable>
     );
 }
