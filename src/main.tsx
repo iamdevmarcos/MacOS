dfb7a3bc35dfa9790c56fdab583f7ee42fbf9136
Marcos Andre <marcosdev.me@gmail.com>
2022-02-28 09:05:44 -0300

    feat: add Global Styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import { GlobalStyles } from "./styles/GlobalStyles";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
+
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    overflow: hidden;
+    font-family: "Work Sans", sans-serif;
+  }
+`;
