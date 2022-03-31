578eaf3052516a648541f0d89efeb5ab70f65ae8
Marcos Andre <marcosdev.me@gmail.com>
2022-03-31 16:01:57 -0300

    style: add link cursor

--- src/components/screen/MenuIcon/styles.ts
+++ src/components/screen/MenuIcon/styles.ts
@@ -1,22 +1,25 @@
 import styled from "styled-components";
 
 type ContainerProps = {
-    icon: string;
-}
+  icon: string;
+};
 
-export const Container = styled.div(({ icon }: ContainerProps) => (`
+export const Container = styled.div(
+  ({ icon }: ContainerProps) => `
     width:50px;
     height:50px;
     background-image: url('/assets/images/icons/appicons/${icon}.png');
     background-position: center;
     background-size:cover;
     margin:10px 7px;
-    cursor:pointer;
     transition: all ease 0.2s;
+
+    cursor: url('/assets/cursors/link.svg'), auto;
     
     &&:hover {
         width:100px;
         height:100px;
         margin-top:-40px; 
     }
-`));
+`
+);
